a39e671da0ae72669e9b84ee2cacbe93795adfed
David S. Batista <dsbatista@gmail.com>
2024-05-10 13:11:49 +0200

    fixing evaluation for SQuAD dataset

--- evaluations/arago_evaluation.py
+++ evaluations/arago_evaluation.py
@@ -1,11 +1,13 @@
 import json
 import os
-from typing import List
 
-from haystack import Pipeline, Document
+from haystack import Pipeline
 from haystack.components.embedders import SentenceTransformersDocumentEmbedder
 from haystack.document_stores.in_memory import InMemoryDocumentStore
+from haystack.components.converters import PyPDFToDocument
 from haystack.components.evaluators import ContextRelevanceEvaluator, FaithfulnessEvaluator, SASEvaluator
+from haystack.components.preprocessors import DocumentCleaner
+from haystack.components.preprocessors import DocumentSplitter
 from haystack.components.writers import DocumentWriter
 from haystack.document_stores.types import DuplicatePolicy
 from haystack.evaluation import EvaluationRunResult
@@ -15,41 +17,33 @@ from architectures.basic_rag import basic_rag
 from architectures.hyde_rag import rag_with_hyde
 
 embedding_model = "sentence-transformers/all-MiniLM-L6-v2"
-base_path = "datasets/SQuAD-2.0/transformed_squad/"
+files_path = "../datasets/ARAGOG/papers_for_questions"
 
 
-def load_transformed_squad():
-    with open(base_path+"questions.jsonl", "r") as f:
-        questions = [json.loads(x) for x in f.readlines()]
-    for idx, question in enumerate(questions):
-        question["query_id"] = f"query_{idx}"
-
-    def create_document(text: str, name: str):
-        return Document(content=text, meta={"name": name})
-
-    # walk through the files in the directory and transform each line of each text file into a Document
-    documents = []
-    for root, dirs, files in os.walk(base_path):
-        for article in files:
-            with open(f"{root}/{article}", "r") as f:
-                raw_texts = f.read().split("\n")
-                for text in raw_texts:
-                    documents.append(create_document(text, article.replace(".txt", "")))
-
-    return questions, documents
-
-
-def indexing(documents: List[Document]):
+def indexing():
     document_store = InMemoryDocumentStore()
-    doc_writer = DocumentWriter(document_store=document_store, policy=DuplicatePolicy.SKIP)
-    doc_embedder = SentenceTransformersDocumentEmbedder(model=embedding_model)
-    ingestion_pipe = Pipeline()
-    ingestion_pipe.add_component(instance=doc_embedder, name="doc_embedder")
-    ingestion_pipe.add_component(instance=doc_writer, name="doc_writer")
-    ingestion_pipe.connect("doc_embedder.documents", "doc_writer.documents")
-    ingestion_pipe.run({"doc_embedder": {"documents": documents}})
+    pipeline = Pipeline()
+    pipeline.add_component("converter", PyPDFToDocument())
+    pipeline.add_component("cleaner", DocumentCleaner())
+    pipeline.add_component("splitter", DocumentSplitter(split_by="sentence", split_length=256))
+    pipeline.add_component("writer", DocumentWriter(document_store=document_store, policy=DuplicatePolicy.SKIP))
+    pipeline.add_component("embedder", SentenceTransformersDocumentEmbedder(embedding_model))
+    pipeline.connect("converter", "cleaner")
+    pipeline.connect("cleaner", "splitter")
+    pipeline.connect("splitter", "embedder")
+    pipeline.connect("embedder", "writer")
+    pdf_files = [files_path+"/"+f_name for f_name in os.listdir(files_path)]
+    pipeline.run({"converter": {"sources": pdf_files}})
 
     return document_store
+
+
+def read_question_answers():
+    with open("../datasets/ARAGOG/eval_questions.json", "r") as f:
+        data = json.load(f)
+        questions = data["questions"]
+        answers = data["ground_truths"]
+    return questions, answers
 
 
 def run_basic_rag(doc_store, sample_questions, sample_answers):
@@ -74,9 +68,10 @@ def run_basic_rag(doc_store, sample_questions, sample_answers):
     results = {
         "context_relevance": context_relevance.run(sample_questions, retrieved_contexts),
         "faithfulness": faithfulness.run(sample_questions, retrieved_contexts, predicted_answers),
-        "sas": sas.run(predicted_answers, sample_answers)
+        "sas": sas.run(predicted_answers, sample_answers),
+        'predicted_answers': predicted_answers,
     }
-    inputs = {'questions': sample_questions, 'answers': predicted_answers}
+    inputs = {'questions': sample_questions, "true_answers": sample_answers, "predicted_answers": predicted_answers}
 
     return EvaluationRunResult(run_name="basic_rag", inputs=inputs, results=results)
 
@@ -98,26 +93,24 @@ def run_hyde_rag(doc_store, sample_questions, sample_answers):
     sas = SASEvaluator(model=embedding_model)
     sas.warm_up()
     results = {
+        'predicted_answers': predicted_answers,
         "context_relevance": context_relevance.run(sample_questions, retrieved_contexts),
         "faithfulness": faithfulness.run(sample_questions, retrieved_contexts, predicted_answers),
         "sas": sas.run(predicted_answers, sample_answers)
     }
-    inputs = {'questions': sample_questions, 'predicted_answers': predicted_answers}
+    inputs = {'questions': sample_questions, "true_answers": sample_answers, "predicted_answers": predicted_answers}
 
     return EvaluationRunResult(run_name="hyde_rag", inputs=inputs, results=results)
 
 
 def main():
-
-    questions, documents = load_transformed_squad()
-    doc_store = indexing(documents)
-
+    doc_store = indexing()
+    questions, ground_truth_answers = read_question_answers()
     limit = 5
-    questions = questions[0:limit]
+    sample_questions = questions[0:limit]
     sample_ground_truth_answers = ground_truth_answers[0:limit]
 
     basic_rag_results = run_basic_rag(doc_store, sample_questions, sample_ground_truth_answers)
     hyde_rag_results = run_hyde_rag(doc_store, sample_questions, sample_ground_truth_answers)
 
     comparative_df = basic_rag_results.comparative_individual_scores_report(hyde_rag_results)
-
