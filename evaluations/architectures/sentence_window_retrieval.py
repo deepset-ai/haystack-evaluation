98e4323514c9c04b352088c4d5d512a518c46ef0
David S. Batista <dsbatista@gmail.com>
2024-06-18 15:58:49 +0200

    adding sentence-window-retrieval to rag pipeline

--- evaluations/architectures/sentence_window_retrieval.py
+++ evaluations/architectures/sentence_window_retrieval.py
@@ -18,9 +18,11 @@ class DocumentChunks:
 
     def __init__(self, chunks: List[Document]):
         self.chunks: List[Document] = chunks
+        self.mappings = {doc.id: idx for idx, doc in enumerate(chunks)}
 
-    def get_window(self, idx: int, window_size: int) -> List[Document]:
-        pass
+    def get_window(self, doc_id: str, window_size: int) -> List[Document]:
+        doc_idx = self.mappings[doc_id]
+        return self.chunks[max(0, doc_idx - window_size):min(len(self.chunks), doc_idx + window_size + 1)]
 
 
 class DocumentChunksStore:
@@ -39,6 +41,7 @@ class DocumentChunker:
     def run(self, documents: List[Document]):
         """
         Stores all the chunks of a document in the same DocumentChunks object.
+
         Each DocumentChunks object is stored in a dictionary with the source_id as key in a DocumentChunksStore object.
         """
         source_id = documents[0].meta['source_id']
@@ -88,3 +91,54 @@ def indexing(embedding_model: str, chunk_size: int, base_path: str) -> Tuple[InM
 
     return doc_store, res['chunker']['doc_chunk_store']
 
+
+@component
+class SentenceWindowRetriever:
+
+    def __init__(self, doc_chunk_store: DocumentChunksStore, window_size: int = 2):
+        self.doc_chunk_store = doc_chunk_store
+        self.window_size = window_size
+
+    @component.output_types(context_windows=List[DocumentChunksStore])
+    def run(self, retrieved_documents: List[Document]):
+        context_windows = []
+        for doc in retrieved_documents:
+            source_id = doc.meta['source_id']
+            document_chunks = self.doc_chunk_store.mappings[source_id]
+            context_windows.append(document_chunks.get_window(source_id, self.window_size))
+        return {"context_windows": context_windows}
+
+
+def rag_sentence_window_retrieval(doc_store, doc_chunk_store, embedding_model, top_k=1):
+    template = """
+        You have to answer the following question based on the given context information only.
+        If the context is empty or just a '\n' answer with None, example: "None".
+
+        Context:
+        {% for document in documents %}
+            {{ document.content }}
+        {% endfor %}
+
+        Question: {{question}}
+        Answer:
+        """
+
+    basic_rag = Pipeline()
+    basic_rag.add_component("query_embedder", SentenceTransformersTextEmbedder(
+        model=embedding_model, progress_bar=False
+    ))
+    basic_rag.add_component("retriever", InMemoryEmbeddingRetriever(doc_store, top_k=top_k))
+    basic_rag.add_component("sentence_window_retriever", SentenceWindowRetriever(doc_chunk_store=doc_chunk_store))
+    basic_rag.add_component("prompt_builder", PromptBuilder(template=template))
+    basic_rag.add_component("llm", OpenAIGenerator(model="gpt-3.5-turbo"))
+    basic_rag.add_component("answer_builder", AnswerBuilder())
+
+    basic_rag.connect("query_embedder", "retriever.query_embedding")
+    basic_rag.connect("retriever", "prompt_builder.documents")
+    basic_rag.connect("retriever", "sentence_window_retriever")
+    basic_rag.connect("prompt_builder", "llm")
+    basic_rag.connect("llm.replies", "answer_builder.replies")
+    basic_rag.connect("llm.meta", "answer_builder.meta")
+    basic_rag.connect("retriever", "answer_builder.documents")
+
+    return basic_rag
--- evaluations/evaluation_sentence_window_retrieval.py
+++ evaluations/evaluation_sentence_window_retrieval.py
@@ -12,6 +12,40 @@ def read_question_answers(base_path: str) -> Tuple[List[str], List[str]]:
     return questions, answers
 
 
+def basic_rag(document_store, embedding_model, top_k=2):
+    template = """
+        You have to answer the following question based on the given context information only.
+        If the context is empty or just a '\n' answer with None, example: "None".
+
+        Context:
+        {% for document in documents %}
+            {{ document.content }}
+        {% endfor %}
+
+        Question: {{question}}
+        Answer:
+        """
+
+    basic_rag = Pipeline()
+    basic_rag.add_component("query_embedder", SentenceTransformersTextEmbedder(
+        model=embedding_model, progress_bar=False
+    ))
+    basic_rag.add_component("retriever", InMemoryEmbeddingRetriever(document_store, top_k=top_k))
+    basic_rag.add_component("prompt_builder", PromptBuilder(template=template))
+    basic_rag.add_component("llm", OpenAIGenerator(model="gpt-3.5-turbo"))
+    basic_rag.add_component("answer_builder", AnswerBuilder())
+
+    basic_rag.connect("query_embedder", "retriever.query_embedding")
+    basic_rag.connect("retriever", "prompt_builder.documents")
+    basic_rag.connect("prompt_builder", "llm")
+    basic_rag.connect("llm.replies", "answer_builder.replies")
+    basic_rag.connect("llm.meta", "answer_builder.meta")
+    basic_rag.connect("retriever", "answer_builder.documents")
+
+    return basic_rag
+
+
+
 def main():
 
     base_path = "../datasets/ARAGOG/"
