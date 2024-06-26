8e05c327b89c11122bb91e08222f1070e2a978a9
David S. Batista <dsbatista@gmail.com>
2024-06-26 16:22:19 +0200

    adding sentence-window-retrieval eval

--- evaluations/architectures/sentence_window_retrieval.py
+++ evaluations/architectures/sentence_window_retrieval.py
@@ -1,120 +1,75 @@
-import os
-from pathlib import Path
-from typing import Tuple, List, Dict
+from typing import List
 
 from haystack import Pipeline, component, Document
 from haystack.components.builders import PromptBuilder, AnswerBuilder
-from haystack.components.converters import PyPDFToDocument
-from haystack.components.embedders import SentenceTransformersTextEmbedder, SentenceTransformersDocumentEmbedder
+from haystack.components.embedders import SentenceTransformersTextEmbedder
 from haystack.components.generators import OpenAIGenerator
-from haystack.components.preprocessors import DocumentCleaner, DocumentSplitter
 from haystack.components.retrievers import InMemoryEmbeddingRetriever
-from haystack.components.writers import DocumentWriter
 from haystack.document_stores.in_memory import InMemoryDocumentStore
-from haystack.document_stores.types import DuplicatePolicy
-
-
-class DocumentChunks:
-
-    def __init__(self, chunks: List[Document]):
-        self.chunks: List[Document] = chunks
-        self.mappings = {doc.id: idx for idx, doc in enumerate(chunks)}
-
-    def get_window(self, doc_id: str, window_size: int) -> List[Document]:
-        doc_idx = self.mappings[doc_id]
-        return self.chunks[max(0, doc_idx - window_size):min(len(self.chunks), doc_idx + window_size + 1)]
-
-
-class DocumentChunksStore:
-
-    def __init__(self, documents: Dict[str, DocumentChunks]):
-        self.mappings = documents
-
-
-@component
-class DocumentChunker:
-
-    def __init__(self):
-        self.documents = {}
-
-    @component.output_types(doc_chunk_store=DocumentChunksStore)
-    def run(self, documents: List[Document]):
-        """
-        Stores all the chunks of a document in the same DocumentChunks object.
-
-        Each DocumentChunks object is stored in a dictionary with the source_id as key in a DocumentChunksStore object.
-        """
-        source_id = documents[0].meta['source_id']
-        chunks = []
-        for doc in documents[0:]:
-            if source_id != doc.meta['source_id']:
-                self.documents[source_id] = DocumentChunks(chunks)
-                print(f"Stored {len(chunks)} chunks for document {source_id}")
-                source_id = doc.meta['source_id']
-                chunks = [doc]
-            else:
-                chunks.append(doc)
-        if chunks:
-            self.documents[source_id] = DocumentChunks(chunks)
-            print(f"Stored {len(chunks)} chunks for document {source_id}")
-
-        return {'doc_chunk_store': DocumentChunksStore(self.documents)}
-
-
-def indexing(embedding_model: str, chunk_size: int, base_path: str) -> Tuple[InMemoryDocumentStore, DocumentChunksStore]:
-    full_path = Path(base_path)
-    files_path = full_path / "papers_for_questions"
-    doc_store = InMemoryDocumentStore()
-    pipeline = Pipeline()
-
-    pipeline.add_component("converter", PyPDFToDocument())
-    pipeline.add_component("cleaner", DocumentCleaner())
-    pipeline.add_component("splitter", DocumentSplitter(split_length=chunk_size))  # splitting by word
-    pipeline.add_component("writer", DocumentWriter(document_store=doc_store, policy=DuplicatePolicy.SKIP))
-    pipeline.add_component("embedder", SentenceTransformersDocumentEmbedder(embedding_model))
-    pipeline.add_component("chunker", DocumentChunker())
-
-    pipeline.connect("converter", "cleaner")
-    pipeline.connect("cleaner", "splitter")
-    pipeline.connect("splitter", "embedder")
-
-    pipeline.connect("splitter", "chunker")
-
-    pipeline.connect("embedder", "writer")
-    pdf_files = [full_path / "papers_for_questions" / f_name for f_name in os.listdir(files_path)]
-    res = pipeline.run({"converter": {"sources": pdf_files}})
-
-    source_ids_doc_store = set([d.meta['source_id'] for d in doc_store.storage.values()])
-    source_ids_doc_chunk_store = res['chunker']['doc_chunk_store'].mappings.keys()
-
-    assert source_ids_doc_store == set(source_ids_doc_chunk_store)
-
-    return doc_store, res['chunker']['doc_chunk_store']
 
 
 @component
 class SentenceWindowRetriever:
 
-    def __init__(self, doc_chunk_store: DocumentChunksStore, window_size: int = 1):
-        self.doc_chunk_store = doc_chunk_store
+    def __init__(self, document_store: InMemoryDocumentStore, window_size: int = 1):
         self.window_size = window_size
+        self.document_store = document_store
 
     @staticmethod
     def get_window_content(docs: List[Document]):
         return ' '.join([doc.content for doc in docs])
 
-    @component.output_types(context_windows=List[DocumentChunksStore])
+    @staticmethod
+    def merge_documents(documents):
+        """
+        Merge a list of doc chunks into a single doc by concatenating their content, eliminating overlapping content.
+        """
+        sorted_docs = sorted(documents, key=lambda doc: doc.meta["split_idx_start"])
+        merged_text = ""
+        last_idx_end = 0
+        for doc in sorted_docs:
+            start = doc.meta["split_idx_start"]  # start of the current content
+
+            # if the start of the current content is before the end of the last appended content, adjust it
+            if start < last_idx_end:
+                start = last_idx_end
+
+            # append the non-overlapping part to the merged text
+            merged_text = merged_text.strip()
+            merged_text += doc.content[start - doc.meta["split_idx_start"]:]
+
+            # update the last end index
+            last_idx_end = doc.meta["split_idx_start"] + len(doc.content)
+
+        return merged_text
+
+    @component.output_types(context_windows=List[Document])
     def run(self, retrieved_documents: List[Document]):
+        """
+        Based on the source_id and on the doc.meta['split_id'] get surrounding documents from the document store
+        """
         context_windows = []
         for doc in retrieved_documents:
             source_id = doc.meta['source_id']
-            document_chunks = self.doc_chunk_store.mappings[source_id]
-            window_text_content = self.get_window_content(document_chunks.get_window(doc.id, self.window_size))
-            context_windows.append(window_text_content)
+            split_id = doc.meta['split_id']
+            min_before = min([i for i in range(split_id-1, split_id-self.window_size-1,-1)])
+            max_after = max([i for i in range(split_id+1, split_id+self.window_size+1, 1)])
+            context_docs = self.document_store.filter_documents(
+                {
+                    "operator": "AND",
+                    "conditions": [
+                        {"field": "source_id", "operator": "==", "value": source_id},
+                        {"field": "split_id", "operator": ">=", "value": min_before},
+                        {"field": "split_id", "operator": "<=", "value": max_after}
+                    ],
+                }
+            )
+            context_windows.append(self.merge_documents(context_docs))
+
         return {"context_windows": context_windows}
 
 
-def rag_sentence_window_retrieval(doc_store, doc_chunk_store, embedding_model, top_k=1):
+def rag_sentence_window_retrieval(doc_store, embedding_model, top_k=1):
     template = """
         You have to answer the following question based on the given context information only.
         If the context is empty or just a '\n' answer with None, example: "None".
@@ -133,7 +88,7 @@ def rag_sentence_window_retrieval(doc_store, doc_chunk_store, embedding_model, t
         model=embedding_model, progress_bar=False
     ))
     basic_rag.add_component("retriever", InMemoryEmbeddingRetriever(doc_store, top_k=top_k))
-    basic_rag.add_component("sentence_window_retriever", SentenceWindowRetriever(doc_chunk_store=doc_chunk_store))
+    basic_rag.add_component("sentence_window_retriever", SentenceWindowRetriever(document_store=doc_store))
     basic_rag.add_component("prompt_builder", PromptBuilder(template=template))
     basic_rag.add_component("llm", OpenAIGenerator(model="gpt-3.5-turbo"))
     basic_rag.add_component("answer_builder", AnswerBuilder())
