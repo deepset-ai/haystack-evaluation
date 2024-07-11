42f1b8422dfc78b61a2dec0c60ed0158df777835
David S. Batista <dsbatista@gmail.com>
2024-07-11 18:46:02 +0200

    wip: TREC PoC

--- evaluations/trec_evaluation.py
+++ evaluations/trec_evaluation.py
@@ -1,18 +1,20 @@
-from collections import defaultdict
-
 import json
 import os
 import re
+from collections import defaultdict
 from dataclasses import dataclass
-from haystack_integrations.document_stores.qdrant import QdrantDocumentStore
+from random import choice
 from typing import Dict, List, Set
 
-from haystack import Pipeline, component, Document
-from haystack.components.embedders import SentenceTransformersDocumentEmbedder
+from haystack import Document, Pipeline, component
+from haystack.components.builders import AnswerBuilder, PromptBuilder
+from haystack.components.embedders import SentenceTransformersDocumentEmbedder, SentenceTransformersTextEmbedder
+from haystack.components.generators import OpenAIGenerator
 from haystack.components.preprocessors import DocumentSplitter
 from haystack.components.writers import DocumentWriter
-from haystack.document_stores.in_memory import InMemoryDocumentStore
 from haystack.document_stores.types import DuplicatePolicy
+from haystack_integrations.components.retrievers.qdrant import QdrantEmbeddingRetriever
+from haystack_integrations.document_stores.qdrant import QdrantDocumentStore
 
 
 @dataclass
@@ -39,10 +41,7 @@ def read_query_relevance(query_relevance_file: str) -> Dict[int, List[QueryRelev
         for line in f:
             query_id, iteration, doc_id, relevance = line.strip().split()
             query = QueryRelevance(
-                topic_id=int(query_id),
-                iteration=iteration,
-                document_id=doc_id,
-                relevance_judgement=int(relevance)
+                topic_id=int(query_id), iteration=iteration, document_id=doc_id, relevance_judgement=int(relevance)
             )
             query_relevance[int(query_id)].append(query)
 
@@ -89,11 +88,11 @@ def read_documents(document_files: Set[str], corpus_path: str):
 def get_qdrant_doc_store(embedding_dim: int = 768):
     doc_store = QdrantDocumentStore(
         url="localhost",
-        index="bundestagwahl",
+        index="trec2024",
         embedding_dim=embedding_dim,
         on_disk=True,
         recreate_index=True,
-        hnsw_config={"m": 16, "ef_construct": 64}  # Optional
+        hnsw_config={"m": 16, "ef_construct": 64},  # Optional
     )
 
     return doc_store
@@ -101,21 +100,20 @@ def get_qdrant_doc_store(embedding_dim: int = 768):
 
 @component
 class ParseTRECCorpus:
-
     @staticmethod
     def create_document(line: str):
         doc = json.loads(line)
-        return Document(content=doc["segment"], meta={"docid": doc['docid'], "url": doc["url"]})
+        return Document(content=doc["segment"], meta={"docid": doc["docid"], "url": doc["url"]})
 
     @component.output_types(segments=List[Document])
     def run(self, files: List[str]):
         for file in files:
             with open(file, "r") as f:
                 results = [self.create_document(line) for line in f]
-        return {'segments': results}
+        return {"segments": results}
 
 
-def indexing(doc_store, model: str, chunk_size: int, files_to_index: Set[str], corpus_path: str):
+def indexing(doc_store, model: str, chunk_size: int, files_to_index: Set[str]):
     pipeline = Pipeline()
     pipeline.add_component("converter", ParseTRECCorpus())
     pipeline.add_component("splitter", DocumentSplitter(split_length=chunk_size, split_overlap=5))  # splitting by word
@@ -124,11 +122,42 @@ def indexing(doc_store, model: str, chunk_size: int, files_to_index: Set[str], c
     pipeline.connect("converter", "splitter")
     pipeline.connect("splitter", "embedder")
     pipeline.connect("embedder", "writer")
-    files = [os.path.join(corpus_path, f_name) for f_name in files_to_index]
-    files = files[0]    # only one file
-    pipeline.run({"converter": {"files": files}})
+    pipeline.run({"converter": {"files": files_to_index}})
 
     return doc_store
+
+
+def built_basic_rag(document_store, embedding_model, top_k=2):
+    template = """
+        You have to answer the following question based on the given context information only.
+        If the context is empty or just a '\n' answer with None, example: "None".
+
+        Context:
+        {% for document in documents %}
+            {{ document.content }}
+        {% endfor %}
+
+        Question: {{question}}
+        Answer:
+        """
+
+    basic_rag = Pipeline()
+    basic_rag.add_component(
+        "query_embedder", SentenceTransformersTextEmbedder(model=embedding_model, progress_bar=False)
+    )
+    basic_rag.add_component("retriever", QdrantEmbeddingRetriever(document_store, top_k=top_k))
+    basic_rag.add_component("prompt_builder", PromptBuilder(template=template))
+    basic_rag.add_component("llm", OpenAIGenerator(model="gpt-3.5-turbo"))
+    basic_rag.add_component("answer_builder", AnswerBuilder())
+
+    basic_rag.connect("query_embedder", "retriever.query_embedding")
+    basic_rag.connect("retriever", "prompt_builder.documents")
+    basic_rag.connect("prompt_builder", "llm")
+    basic_rag.connect("llm.replies", "answer_builder.replies")
+    basic_rag.connect("llm.meta", "answer_builder.meta")
+    basic_rag.connect("retriever", "answer_builder.documents")
+
+    return basic_rag
 
 
 def prepare():
@@ -142,4 +171,18 @@ def prepare():
     model = "all-MiniLM-L12-v2"
     embedding_dim = 384
     doc_store = get_qdrant_doc_store(embedding_dim)
-    indexing(doc_store, model, 128, files_to_index, "../TREC/corpus/")
+    indexing(doc_store, model, 128, files_to_index)
+
+    rag = built_basic_rag(doc_store, model, top_k=2)
+
+    # choose a random entry from the topics
+    random_key = choice(list(topics.keys()))
+    question = topics[random_key]
+
+    rag.run(
+        {
+            "query_embedder": {"text": question},
+            "prompt_builder": {"question": question},
+            "answer_builder": {"query": question},
+        }
+    )
